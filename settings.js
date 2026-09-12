2f1342144b360ec6c2cbbd43897320d163835feb
Abdulaziz khalifa <146680488+Abdul3ziz95@users.noreply.github.com>
2026-09-12 03:44:16 +0300

    تحديث الفروع من لوحة التحكم 🍝

--- settings.js
+++ settings.js
@@ -209,8 +209,8 @@ window.APP_SETTINGS = {
       "addrAr": "القادسية، الياسمين، الرياض 13322",
       "addrEn": "Al Qadisiyah, Al Yasmeen, Riyadh 13322",
       "wa": "966561203593",
-      "lat": 24.86,
-      "lng": 46.64,
+      "lat": 24.817363,
+      "lng": 46.656311,
       "map": "https://maps.app.goo.gl/WxasXA9oWLbSTCjq8",
       "hours": [
         {
